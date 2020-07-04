93e2640bd670586cf205aa818baca5fcf5cb8a7c
jane1002 <jane.fan1002@gmail.coom>
2020-07-04 02:32:47 -0500

    Add eslint config.

--- src/Hello.ts
+++ src/Hello.ts
@@ -1,14 +1,14 @@
 class Hello {
-    firstName : string;
-    lastName : string;
-    constructor(fiestName : string,  lastName : string) {
-        this.firstName = fiestName;
-        this.lastName = lastName;
+    firstName: string;
+    lastName: string;
+    constructor (fiestName: string, lastName: string) {
+      this.firstName = fiestName
+      this.lastName = lastName
     }
-    greeter() {
-        return "欢迎来到typescript的世界，hello" +
-         this.firstName + " " + this.lastName;
+
+    greeter () {
+      return `欢迎来到typescript的世界，hello${this.firstName} ${this.lastName}`
     }
 }
-var user = new Hello("王", "小二");
-document.body.innerHTML = user.greeter();
+var user = new Hello('王', '小二')
+document.body.innerHTML = user.greeter()
